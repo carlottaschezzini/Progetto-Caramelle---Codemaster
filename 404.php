5e6a484cbe6bd9224bbc9abc92a1c0acb7c26c27
carlottaschezzini <carlottaschezzini@gmail.com>
2016-03-13 16:55:55 +0100

    Added files via upload
    aggiornamento 404

---
+++ 404.php
@@ -0,0 +1,28 @@
+<?php $logged = TRUE; ?> 
+<!doctype html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>404</title>
+
+<?php include ("include/content.php");?>
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
+
+</head>
+
+<body>
+
+<?php include ("include/header1.php");?>
+        <div class="col-sm-12 errorpage container">  
+            <div class="tag">
+            <span><strong>TI SEI PERSO? </strong>
+   							CLICCA QUI!</span>
+            
+            </div>
+        </div>
+
+        
+
+</body>
+</html>
