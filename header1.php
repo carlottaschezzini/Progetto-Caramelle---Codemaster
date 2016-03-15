ce9434e315bb1b66be3abec687a528e54160873f
nicopunzi <nicopunzi@tiscali.it>
2016-03-15 19:17:36 +0100

    Added files via upload

---
+++ header1.php
@@ -0,0 +1,27 @@
+
+<header class="clearfix blue">
+
+    <div class="container clearfix">
+            <div class="header-left">
+             <a href="area_utenti.php"><img  style=" margin-left: 1%;
+    margin-top: 1%;"src="img/logo_home.png"></a>
+            </div>
+            <div class="header-right">
+                <label for="open">
+                    <span class="hidden-desktop"></span>
+                </label>
+                <input type="checkbox" name="" id="open">
+                <nav style="padding-top: 6%;">
+   
+                     <a href="home.php fontstyle">PRENOTA UN'AULA</a>
+                        <a href="calendario_eventi.php">CALENDARIO EVENTI</a>
+                      <a href="calendario_eventi.php">LE MIE PRENOTAZIONI</a>
+   
+  </div>
+                </nav>
+
+                            </div>
+        </div>
+    </header>
+    </br>
+    </br>
