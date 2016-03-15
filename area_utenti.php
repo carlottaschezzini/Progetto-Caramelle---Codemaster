658b02521657e8a48d84e726c3e847b234eb5d79
nicopunzi <nicopunzi@tiscali.it>
2016-03-15 19:23:33 +0100

    Added files via upload

--- area_utenti.php
+++ area_utenti.php
@@ -3,6 +3,8 @@
 <head>
 <meta charset="UTF-8">
 <title>AREA UTENTI - HOME PAGE</title>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+
 <?php include ("include/content.php");?>
 </head>
 
